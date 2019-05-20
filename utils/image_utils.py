208fabdb2d25b828612d15f6e441dc25ad240a71
idearibosome <idearibosome@gmail.com>
2019-05-20 21:31:07 +0900

    Add image manipulation utils

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -48,3 +49,35 @@ class ImageWriter:
   def write(self, image, image_path):
     self.tf_session.run(self.tf_write_op, feed_dict={self.tf_image:image, self.tf_image_path:image_path})
 
+
+class ImageManipulator:
+
+  def __init__(self):
+    pass
+  
+  def match_size(self, image1, image2):
+    image1_shape = image1.shape
+    image2_shape = image2.shape
+
+    image1 = image1[:min(image1_shape[0], image2_shape[0]), :min(image1_shape[1], image2_shape[1])]
+    image2 = image2[:min(image1_shape[0], image2_shape[0]), :min(image1_shape[1], image2_shape[1])]
+
+    return image1, image2
+  
+  def shave_border(self, image, amount):
+    image = image[amount:-amount, amount:-amount]
+    return image
+  
+  def rgb_to_ycbcr(self, rgb):
+    ycbcr = np.zeros_like(rgb)
+
+    r = rgb[:, :, 0]
+    g = rgb[:, :, 1]
+    b = rgb[:, :, 2]
+
+    ycbcr[:, :, 0] = 16.0 + (65.481/255.0 * r) + (128.553/255.0 * g) + (24.966/255.0 * b)
+    ycbcr[:, :, 1] = 128.0 - (37.797/255.0 * r) - (74.203/255.0 * g) + (112.0/255.0 * b)
+    ycbcr[:, :, 2] = 128.0 - (112.0/255.0 * r) - (93.786/255.0 * g) - (18.214/255.0 * b)
+
+    return ycbcr
+  
